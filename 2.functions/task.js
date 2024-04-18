995e6fe63bdbeb7e4cab147ece320f5c895e8c81
MarniaTu <147163020+MarniaTu@users.noreply.github.com>
2024-04-18 11:45:57 +0300

    решение задания по теме Функции

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,108 @@
-function getArrayParams(...arr) {
+"use strict";
 
-  return { min: min, max: max, avg: avg };
+function getArrayParams(...arr) {
+	let min = arr[0];
+	let max = arr[0];
+	let sum = 0;
+
+
+	for (let i = 0; i < arr.length; i++) {
+
+		if (arr[i] > max) {
+			max = arr[i];
+		} else if (arr[i] < min) {
+			min = arr[i];
+		}
+
+		sum += arr[i];
+	}
+
+	let avg = sum / arr.length;
+	avg = +avg.toFixed(2);
+
+	return {
+		min: min,
+		max: max,
+		avg: avg
+	};
 }
+
 
 function summElementsWorker(...arr) {
+	let sum = 0;
 
+	for (let i = 0; i < arr.length; i++) {
+
+		if (arr.length === 0) {
+			return 0;
+		} else
+			sum += arr[i];
+	}
+	return sum;
 }
+  
 
 function differenceMaxMinWorker(...arr) {
+	let min = Math.min(...arr);
+	let max = Math.max(...arr);
+	const d = max - min;
+
+	if (arr.length === 0) {
+		return 0;
+
+	} else return d;
 
 }
 
 function differenceEvenOddWorker(...arr) {
+	let sumEvenElement = 0;
+	let sumOddElement = 0;
 
+	for (let i = 0; i < arr.length; i++) {
+		if (arr.length === 0) {
+			return 0;
+
+		} else if (arr[i] % 2 === 0) {
+			sumEvenElement += arr[i];
+
+		} else 
+			sumOddElement += arr[i];
+  }
+		const d = sumEvenElement - sumOddElement;
+		return d;
+	
 }
 
 function averageEvenElementsWorker(...arr) {
+	let sumEvenElement = 0;
+	let countEvenElement = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+
+		if (arr.length === 0) {
+			return 0;
+		} else if (arr[i] % 2 === 0) {
+
+			sumEvenElement += arr[i];
+			countEvenElement++;
+		}
+	}
+	let avg = sumEvenElement / countEvenElement;
+	return avg;
 
 }
 
-function makeWork (arrOfArr, func) {
+function makeWork(arrOfArr, func) {
+	let maxWorkerResult = -Infinity;
+	const arr = [...arrOfArr];
+
+	for (let i = 0; i < arr.length; i++) {
+		const result = func(arr[i]);
+
+		if (result > maxWorkerResult) {
+			maxWorkerResult = result;
+		}
+	}
+	return maxWorkerResult;
 
 }
