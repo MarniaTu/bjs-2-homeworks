e226c39ae442cd19da067a39bb0dacce79453a87
MarniaTu <147163020+MarniaTu@users.noreply.github.com>
2024-04-19 16:47:33 +0300

    modify code in function averageEvenElementsWorker

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -77,11 +77,12 @@ function averageEvenElementsWorker(...arr) {
 	let sumEvenElement = 0;
 	let countEvenElement = 0;
 
+	if (arr.length === 0) {
+		return 0;
+	}
 	for (let i = 0; i < arr.length; i++) {
 
-		if (arr.length === 0) {
-			return 0;
-		} else if (arr[i] % 2 === 0) {
+		if (arr[i] % 2 === 0) {
 
 			sumEvenElement += arr[i];
 			countEvenElement++;
